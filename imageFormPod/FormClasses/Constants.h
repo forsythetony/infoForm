6a4a691b0e0f8d77965ef1c733e78f06c9e39ecd
forsythetony <forsythetony@gmail.com>
2014-08-05 11:58:48 -0500

    h

---
+++ imageFormPod/FormClasses/Constants.h
@@ -0,0 +1,13 @@
+//
+//  Constants.h
+//  PhotoUploading
+//
+//  Created by Anthony Forsythe on 8/5/14.
+//  Copyright (c) 2014 Tonyf. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+extern NSString * const keyDateTaken;
+extern NSString * const keyDateUploaded;
+extern NSString * const keyPhotographerName;
---
+++ imageFormPod/FormClasses/Constants.m
@@ -0,0 +1,13 @@
+//
+//  Constants.m
+//  PhotoUploading
+//
+//  Created by Anthony Forsythe on 8/5/14.
+//  Copyright (c) 2014 Tonyf. All rights reserved.
+//
+
+#import "Constants.h"
+
+NSString * const keyDateTaken = @"keyDateTakenJSON";
+NSString * const keyDateUploaded = @"keyDateUploadedJSON";
+NSString * const keyPhotographerName = @"keyPhotographerNameJSON";
